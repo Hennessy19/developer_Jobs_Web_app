8637c6a61fc1648f088d9665a2e6d261f731e4c6
Samson Wanendeya <nateniel18@gmail.com>
2025-02-08 11:41:29 +0300

    Added react router.
    Also removed pages from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
+import { Route, createBrowserRouter, createRoutesFromElements , RouterProvider, Router } from "react-router-dom"
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      {/* <Route path="job-listings" element={<JobListings />} /> */}
+      {/* <Route path="view-all-jobs" element={<ViewAllJobs />} /> */}
+    </Route>
+ 
+)
+);
+
 
 const App = () =>{
-    return (
-        <>
-        <Navbar />
-        <Hero />
-        <HomeCards />
-        <JobListings />
-        <ViewAllJobs />
-        </>
-    )
+    return <RouterProvider router={router}/>;
 };
 
 export default App;
