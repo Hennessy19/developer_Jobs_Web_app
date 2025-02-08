157d12a66ae2a196985787a21c7ffcca2d618149
Samson Wanendeya <nateniel18@gmail.com>
2025-02-08 12:15:58 +0300

    Add routing for jobs and not found pages; update navigation links to use React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Route, createBrowserRouter, createRoutesFromElements , RouterProvider, Router } from "react-router-dom"
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
+import NotFound from "./pages/NotFound";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path="jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFound />} />
+
       {/* <Route path="job-listings" element={<JobListings />} /> */}
       {/* <Route path="view-all-jobs" element={<ViewAllJobs />} /> */}
     </Route>
